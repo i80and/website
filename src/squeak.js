a4a867ccdfa0afd65ce928c69903e57438722a87
Heli Aldridge <heli@heli.pet>
2021-07-19 20:23:41 -0500

    improve squeaks

--- src/squeak.js
+++ src/squeak.js
@@ -1,30 +1,35 @@
 (() => {
     "use strict";
     let squeaks = 0;
-    document.body.onmousedown = (ev) => {
-        if (ev.target.tagName == "A") {
-            return true;
-        }
 
-        squeaks += 1;
-        const element = document.createElement("div");
-        element.className = "click-doodad";
-        if (squeaks > 3) {
-            const extraSqueaks = squeaks - 3;
-            element.innerText = "squ" + "~".repeat(extraSqueaks) + "💜".repeat(extraSqueaks) + "~".repeat(extraSqueaks) + "ak";
+    const spawnSqueak = (x, y) => {
+        squeaks += 1
+        const element = document.createElement("div")
+        element.className = "click-doodad"
+        if (squeaks > 2) {
+            const extraSqueaks = Math.min(squeaks - 2, 3)
+            element.innerText = "squ" + "~".repeat(extraSqueaks) + "💜".repeat(extraSqueaks) + "~".repeat(extraSqueaks) + "ak"
         } else {
             element.innerText = "sque" + "e".repeat(Math.random() * 5) + "ak!" + "!".repeat(Math.random() * 4)
         }
-        element.style.left = `calc(${ev.clientX}px - 1.5em)`
-        element.style.top = `calc(${ev.clientY}px - 0.5em)`
-        document.body.appendChild(element);
+        element.style.left = `calc(${x}px - 1.5em)`
+        element.style.top = `calc(${y}px - 0.5em)`
+        document.body.appendChild(element)
         setTimeout(() => {
-            document.body.removeChild(element);
-            squeaks -= 1;
+            document.body.removeChild(element)
+            squeaks -= 1
         }, 1000)
         setTimeout(() => {
-            element.style.transform = 'translateY(-50px)';
-            element.style.opacity = "0";
+            element.style.transform = 'translateY(-50px)'
+            element.style.opacity = "0"
         }, 1)
     }
-})();
+
+    document.body.onmousedown = (ev) => {
+        if (ev.button !== 0 || ["A", "IMG"].indexOf(ev.target.tagName) >= 0) {
+            return
+        }
+
+        spawnSqueak(ev.clientX, ev.clientY)
+    }
+})()
